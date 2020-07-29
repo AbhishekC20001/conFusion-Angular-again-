abc39336040c9b3053416532b5f0fba8daf0444d
Abhishek Chopra <abhishekachopra7@gmail.com>
2020-07-29 20:55:41 +0530

    Angular Template driven forms part 2

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 import { Feedback, ContactType } from '../shared/feedback';
 @Component({
@@ -13,6 +15,8 @@ export class ContactComponent implements OnInit {
   feedback: Feedback;
   contactType = ContactType;
 
+  @ViewChild('fform') feedbackFormDirective;
+
   constructor(private fb: FormBuilder) {
     this.createForm();
    }
@@ -21,20 +25,28 @@ export class ContactComponent implements OnInit {
   }
   createForm() {
     this.feedbackForm = this.fb.group({
-      firstname: '',
-      lastname: '',
-      telnum: 0,
-      email: '',
+      firstname: ['', Validators.required ],
+      lastname: ['', Validators.required ],
+      telnum: ['', Validators.required ],
+      email: ['', Validators.required ],
       agree: false,
       contacttype: 'None',
       message: ''
     });
   }
-
   onSubmit() {
     this.feedback = this.feedbackForm.value;
     console.log(this.feedback);
-    this.feedbackForm.reset();
+    this.feedbackForm.reset({
+      firstname: '',
+      lastname: '',
+      telnum: '',
+      email: '',
+      agree: false,
+      contacttype: 'None',
+      message: ''
+    });
+    
   }
 
 }
