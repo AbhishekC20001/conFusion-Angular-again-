e6cde2900ba6cf55fdb64f225b5da93d0b1cbf65
Abhishek Chopra <abhishekachopra7@gmail.com>
2020-08-02 12:43:35 +0530

    Custom Attribute Directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -45,6 +45,7 @@ import { LeaderService } from './services/leader.service';
 import { LoginComponent } from './login/login.component';
 
 import { baseURL } from './shared/baseurl';
+import { HighlightDirective } from './directives/highlight.directive';
 
 
 @NgModule({
@@ -57,7 +58,8 @@ import { baseURL } from './shared/baseurl';
     AboutComponent,
     HomeComponent,
     ContactComponent,
-    LoginComponent
+    LoginComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
